09a3f5642b80cfffa4041dedd43687a9f64e471c
Jin <>
2023-12-22 06:48:26 +0800

    Update template editor to sync with zustand store

--- components/template-editor.tsx
+++ components/template-editor.tsx
@@ -3,16 +3,29 @@
 import { Box, Input, Stack, TextInput } from '@mantine/core'
 import MDEditor from '@uiw/react-md-editor'
 import React from 'react'
+import { useAppDataStore } from '~/lib/hooks'
 
 export const TemplateEditor: React.FC = () => {
-  const [value, setValue] = React.useState<string | undefined>('')
+  const subjectTemplate = useAppDataStore((state) => state.subjectTemplate)
+  const mdTemplate = useAppDataStore((state) => state.mdTemplate)
   return (
     <Stack gap='md' pt='md'>
-      <TextInput label='Subject template' />
+      <TextInput
+        label='Subject template'
+        value={subjectTemplate}
+        onChange={(event) =>
+          useAppDataStore.setState({ subjectTemplate: event.currentTarget.value })
+        }
+      />
       <Stack gap='2'>
         <Input.Label>Mail body template</Input.Label>
         <Box data-color-mode='light'>
-          <MDEditor value={value} onChange={setValue} preview='edit' height={300} />
+          <MDEditor
+            value={mdTemplate}
+            onChange={(value) => useAppDataStore.setState({ mdTemplate: value ?? '' })}
+            preview='edit'
+            height={300}
+          />
         </Box>
       </Stack>
     </Stack>
