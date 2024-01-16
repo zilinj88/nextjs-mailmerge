b91a648846f921ebc3e765c0d69f015fe5f5ab51
Jin <>
2024-01-17 05:53:20 +0800

    sss

--- components/users-table.tsx
+++ components/users-table.tsx
@@ -20,9 +20,6 @@ const UserRowComp: React.FC<UserRowCompProps> = ({ index }) => {
     return null
   }
   const row = atOrThrow(data.rows, index)
-  const selectRow = (index: number) => {
-    useAppDataStore.setState({ selectedIndex: index })
-  }
   return (
     <Table.Tr
       key={index}
